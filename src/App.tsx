955e7a7557034a640688ddac16f0dacb1ca3d7a8
Paulo Henrique de Barros Brandão <paulobrandaohbb@gmail.com>
2026-02-11 17:23:17 -0300

    Atualizaçao no blogpessoal frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home"; 
 
 function App (){
 
   return(
-  <Home/>
-);
+    <>
+     <Navbar/>
+     <Home/>
+     <Footer/>
+     </>
+  );
 }
 export default App;
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+function Navbar(){
+
+  return( 
+         <div className='w-full flex justify-center py-4
+                          bg-indigo-900 text-white'>
+             <div className = 'container flex justify-between text-lg mx-8 font-bold'>
+              Blog Pessoal
+            <div className='flex gap-4 font-bold '>
+               Postagem
+               Tema
+               Cadastrar Tema
+               Perfil 
+               Sair
+          </div>
+       </div>
+    </div>
+  )
+}
+export default Navbar
